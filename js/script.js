085bdf57f0fdcb33f8958eeeb4b44792ab1e45d1
vijpra <93149661+vijpra@users.noreply.github.com>
2024-07-08 00:47:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+function changeReadMore(id) { 
+	const mycontent = 
+		document.getElementById('more-' + id); 
+	const mybutton = 
+		document.getElementById('more-button-' + id); 
+	const dots = document.getElementById("dots") 
+
+	if (mycontent.style.display === 'none'
+		|| mycontent.style.display === '') { 
+		mycontent.style.display = 'inline'; 
+		dots.style.display = "none"; 
+		mybutton.textContent = 'Read Less'; 
+	} else { 
+		mycontent.style.display = 'none'; 
+        mybutton.textContent = 'Read More'; 
+        dots.style.display = "inline"; 
+       
+		
+	} 
+} 
 
